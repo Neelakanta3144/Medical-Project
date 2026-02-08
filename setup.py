c924860606082d233a88289bdcff7b314c24b668
Neelakanta3144 <neelakantasapare@gmail.com>
2026-02-08 12:27:34 +0530

    Project setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Neelakanta Sapare",
+    author_email="neelakantasapare3@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
